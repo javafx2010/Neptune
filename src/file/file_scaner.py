b8e14c38a58f038c4f3f2b94abea2444a22868aa
javafx <javafx2010@icloud.com>
2017-05-20 14:07:56 +0800

    counter

--- src/file/file_scaner.py
+++ src/file/file_scaner.py
@@ -10,9 +10,12 @@ class FileScaner:
         self.writer = writer
 
     def scan(self):
+        counter = 0
         class_if = classif.classif()
         for root, dirs, files in os.walk(self.path):
             for file in files:
                 if file.endswith(self.exts):
                     tag = class_if.action(file)
                     self.writer.put(file, tag)
+                    counter += 1
+        return counter
--- src/main.py
+++ src/main.py
@@ -4,4 +4,5 @@ from src.file import file_writer
 if __name__ == "__main__":
     writer = file_writer.FileWriter("/Volumes/XiaoMi/TDDOWNLOAD")
     scaner = file_scaner.FileScaner("/Volumes/XiaoMi/图片/home", ".JPG", writer)
-    scaner.scan()
+    counter = scaner.scan()
+    print(counter)
