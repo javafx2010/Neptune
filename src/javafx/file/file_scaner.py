985c64a2f4b14d96ae80ff9784c59e3118c9281a
javafx <javafx2010@icloud.com>
2017-05-27 00:30:00 +0800

    去重复任务

--- src/javafx/file/file_scaner.py
+++ src/javafx/file/file_scaner.py
@@ -1,5 +1,6 @@
+#-*-conding:utf-8-*-
 import os
-
+import codecs
 from src.javafx.image import classif
 
 
@@ -8,16 +9,48 @@ class FileScaner:
         self.path = path
         self.writer = writer
 
+    charset="UTF-8"
+
+    def load(self,path):
+        if os.path.exists(path):
+            file = codecs.open(path, "r", self.charset)
+            ls = file.readlines()
+            for i in range(len(ls)):
+                ls[i] = ls[i].replace("\n", "")
+                print(ls[i])
+            file.close()
+            self.lines = ls
+        else:
+            self.lines=[]
+
+    def skip(self,path):
+        lines=self.lines
+        for i in range(len(lines)):
+            if lines[i] == path:
+                return 1
+        return 0
+
     def scan(self):
         counter = 0
+
+        self.load(self.path+"/../process")
+
+        process=codecs.open(self.path+"/../process","w",self.charset)
         class_if = classif.classif()
         for root, dirs, files in os.walk(self.path):
             for file in files:
                 if os.path.isfile(os.path.join(root, file)):
                     full_path = os.path.join(root, file)
+                    if self.skip(full_path)==1:
+                        print("skip:\t"+full_path)
+                        continue
                     self.writer.put(full_path)
-                    counter += 1
-        self.writer.flush()
+                    process.write(full_path+"\n")
+                    process.flush()
 
+                    counter += 1
+
+        self.writer.flush()
+        process.close()
         return counter
 
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 #!/usr/local/bin/python3
-
 from src.javafx.file import file_scaner
 from src.javafx.log import logger
-from src.javafx.file import  file_writer
+from src.javafx.file import file_writer
 
 if __name__ == "__main__":
     log = logger.Logger("/Users/javafx/Work")
     writer = file_writer.FileWriter("/Volumes/XiaoMi/one", log)
     scaner = file_scaner.FileScaner("/Volumes/XiaoMi/图片/home", writer)
+
     counter = scaner.scan()
     print(counter)
